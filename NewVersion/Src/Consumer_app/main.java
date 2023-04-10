e462a9ae2d17151d36713bde5607ccae05e88990
Abdulmalik999 <abdulmalikbm3@gmail.com>
2023-04-10 18:05:09 -0400

    create dir for each app

---
+++ NewVersion/Src/Consumer_app/main.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import akka.actor.typed.ActorSystem;
+
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args) {
+		ArrayList<String> Stocks = new ArrayList<>();
+		double budget = 10000;
+		Stocks.add("OthaimMarket");
+		Stocks.add("AlrijhiBanak");
+		Stocks.add("ACC");
+		Stocks.add("ACWAPower");
+		Stocks.add("Advanced");
+		Consumer c = new Consumer("x", "Consumer5");
+		ActorSystem<TradeManager.Stocks> TreaderSystem = ActorSystem.create(TradeManager.create(),"TradeManager");
+		TreaderSystem.tell(new TradeManager.CreateTrade(Stocks, budget));
+		while (true){
+			ArrayList<TradeManager.StocksPriceReciver> value =  c.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+		}
+		/*
+		org.example.Consumer c1 = new org.example.Consumer("x", 0, "ConsumerGroup3");
+		org.example.Consumer c2 = new org.example.Consumer("x", 1, "ConsumerGroup3");
+		org.example.Consumer c3 = new org.example.Consumer("x", 2, "ConsumerGroup3");
+		org.example.Consumer c4 = new org.example.Consumer("x", 3, "ConsumerGroup3");
+		org.example.Consumer c5 = new org.example.Consumer("x", 4, "ConsumerGroup3");
+		// TODO Auto-generated method stub
+		ActorSystem<Traders.StockMarket> TreaderSystem = ActorSystem.create(Traders.create(),"TreaderActor");
+		while(true){
+			ArrayList<Traders.StockMarket> value =  c1.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+			value =  c2.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+			value =  c3.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+			value =  c4.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+			value =  c5.Consume();
+			for(int i=0;i<value.size();i++) {
+				TreaderSystem.tell(value.get(i));
+			}
+		}
+		*/
+	}
+	
+}
